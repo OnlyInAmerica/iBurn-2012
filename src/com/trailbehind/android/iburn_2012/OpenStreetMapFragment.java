e0000143be0b9606ca634a6e4ade79d5d99617bd
David Brodsky <davidpbrodsky@gmail.com>
2012-08-15 17:33:16 -0700

    initial commit

---
+++ src/com/trailbehind/android/iburn_2012/OpenStreetMapFragment.java
@@ -0,0 +1,72 @@
+package com.trailbehind.android.iburn_2012;
+import org.osmdroid.DefaultResourceProxyImpl;
+import org.osmdroid.ResourceProxy;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.MyLocationOverlay;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+* Based on osmdroid default map view activity.
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+* 
+* Default map view activity.
+* @author Manuel Stahl
+*
+*/
+
+public class OpenStreetMapFragment extends Fragment {
+    /** Called when the activity is first created. */
+    private MapController mapController;
+    private MapView mapView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        
+    }
+    
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+    	
+    	View view = inflater.inflate(R.layout.map, null);
+    	/*
+    	mapView = (MapView) view.findViewById(R.id.mapview);
+    	mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.setBuiltInZoomControls(true);
+        mapController = mapView.getController();
+        mapController.setZoom(15);
+        GeoPoint point2 = new GeoPoint(51496994, -134733);
+        mapController.setCenter(point2);
+    	*/
+    	return view;
+    }
+    
+    protected boolean isRouteDisplayed() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+}   
